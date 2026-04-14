40495d0364b632c9f9cbaf31e17d79795bc2e29f
Yuu Mac <yutomica@gmail.com>
2026-04-14 14:23:19 +0900

    config修正

--- scripts/run_rough_tuning.sh
+++ scripts/run_rough_tuning.sh
@@ -70,9 +70,9 @@ print(run.info.run_id)
 }
 
 # -- LGBM
-run_sweep "tac" "lgbm" "tac_vol_scaled_asym_return" "features_lgbm_tac_vol_scaled_asym_return_rough" \
-    ++hparams.custom_objective="src.models.custom_objectives.custom_asymmetric_mse" \
-    ++hparams.custom_metric="src.models.custom_objectives.custom_asymmetric_mse_eval"
+# run_sweep "tac" "lgbm" "tac_vol_scaled_asym_return" "features_lgbm_tac_vol_scaled_asym_return_rough" \
+#     ++hparams.custom_objective="src.models.custom_objectives.custom_asymmetric_mse" \
+#     ++hparams.custom_metric="src.models.custom_objectives.custom_asymmetric_mse_eval"
 
 # run_sweep "tac" "lgbm" "tac_max_neg_path" "features_lgbm_tac_max_neg_path_rough" \
 #     ++hparams.objective="quantile" \
@@ -80,15 +80,15 @@ run_sweep "tac" "lgbm" "tac_vol_scaled_asym_return" "features_lgbm_tac_vol_scale
 #     ++hparams.alpha=0.1 \
 #     ++hparams.min_child_samples=10
 
-# run_sweep "str" "lgbm" "str_sharpe_adj" "features_lgbm_str_sharpe_adj_rough" \
-#     ++hparams.objective="fair" \
-#     ++hparams.metric="fair" \
-#     ++hparams.fair_c=10.0
+run_sweep "str" "lgbm" "str_sharpe_adj" "features_lgbm_str_sharpe_adj_rough" \
+    ++hparams.objective="fair" \
+    ++hparams.metric="fair" \
+    ++hparams.fair_c=10.0
 
-# run_sweep "str" "lgbm" "str_mdd" "features_lgbm_str_mdd_rough" \
-#     ++hparams.objective="tweedie" \
-#     ++hparams.metric="tweedie" \
-#     ++hparams.tweedie_variance_power=1.2
+run_sweep "str" "lgbm" "str_mdd" "features_lgbm_str_mdd_rough" \
+    ++hparams.objective="tweedie" \
+    ++hparams.metric="tweedie" \
+    ++hparams.tweedie_variance_power=1.2
 
 # -- TCN
 # run_sweep "tac" "tcn" "tac_vol_scaled_asym_return" "features_tcn_tac_vol_scaled_asym_return_rough" \
@@ -106,13 +106,13 @@ run_sweep "tac" "lgbm" "tac_vol_scaled_asym_return" "features_lgbm_tac_vol_scale
 #     ++hparams.objective="fair" \
 #     ++hparams.metric="fair" \
 #     ++hparams.fair_c=10.0 \
-#     +hydra.sweeper.params.model.window_size.str="choice(90,126,252)"
+#     +hydra.sweeper.params.model.window_size.str="choice(126,252)"
 
 # run_sweep "str" "tcn" "str_mdd" "features_tcn_str_mdd_rough" \
 #     ++hparams.objective="tweedie" \
 #     ++hparams.metric="tweedie" \
 #     ++hparams.tweedie_variance_power=1.2 \
-#     +hydra.sweeper.params.model.window_size.str="choice(90,126,252)"
+#     +hydra.sweeper.params.model.window_size.str="choice(126,252)"
 
 # -- FT-Transformer
 # run_sweep "tac" "ft_transfomer" "tac_vol_scaled_asym_return" "features_ft_transfomer_tac_vol_scaled_asym_return_rough" \
